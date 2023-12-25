6de4feb881b7c2bca36189aa9c84b45e75717c1e
DanielK <daniel.kyndl2@seznam.cz>
2023-12-25 15:47:04 +0100

    Mobile support

--- js/modules/simulation.js
+++ js/modules/simulation.js
@@ -15,44 +15,60 @@ export const beginSimulation = () => {
 const humanGame = meta => {
 
 	meta = initPlayerGame(meta)
-
+	const events = ['click','keydown']
 
 	// Main Game Loop
-	window.addEventListener("keydown", e => {
+	events.forEach(evt => window.addEventListener(evt, e => {
 
 		let gameState;
-		const keycode = e.key.replace("Arrow", "").toLowerCase()
-		if (["up", "down", "left", "right"].includes(keycode)) {
-			gameState = meta.game[keycode]()
-		} else return;
+		if (evt === "keydown") {
+			const keycode = e.key.replace("Arrow", "").toLowerCase()
+			if (["up", "down", "left", "right"].includes(keycode)) {
+				gameState = meta.game[keycode]()
+			} else return;
+		}
+		if (evt === 'click' && e.target.hasAttribute("js-mobile-controls")) {
+			gameState = meta.game[e.target.getAttribute("js-mobile-controls")]()
+		}
 
-		if (gameState.status === 100) {
-			Environment.score = meta.game.board.score;
+		if (gameState) {
 
-			if (meta.game.board.score > meta.maxScore) {
-				meta.maxScore = meta.game.board.score
-				Environment.pb = meta.maxScore
+			if (gameState.status === 100) {
+				Environment.score = meta.game.board.score;
+	
+				if (meta.game.board.score > meta.maxScore) {
+					meta.maxScore = meta.game.board.score
+					Environment.pb = meta.maxScore
+				}
+	
+				if (!meta.game.board.spawnTile()) {
+					Environment.endgame(meta.game)
+					gameState.status = 600
+				}
+	
 			}
 
-			if (!meta.game.board.spawnTile()) {
-				Environment.endgame(meta.game)
-				gameState.status = 600
-			}
+		return
 
 		}
 
-	});
+		if (e.target.hasAttribute('js-restart')) {
 
+			meta.board.cleanup()
+			Environment.hideTitleScreen()
+			Environment.score = 0;
+			meta = initPlayerGame(meta)
 
-	// Control Buttons:
-	Environment.restart.forEach(btn => btn.addEventListener("click", () => {
-
-		meta.board.cleanup()
-		Environment.hideTitleScreen()
-		Environment.score = 0;
-		meta = initPlayerGame(meta)
+		}
 
 	}));
+
+
+	// // Control Buttons:
+	// Environment.restart.forEach(btn => btn.addEventListener("click", () => {
+
+
+	// }));
 
 }
 
