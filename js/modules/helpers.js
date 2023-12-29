d51d8b8dfd238f6232cddc2e8e1576d9fcb95381
DanielK <daniel.kyndl2@seznam.cz>
2023-12-29 12:40:32 +0100

    Expansion of utile function library

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -26,9 +26,21 @@ export const initLiveGame = meta => {
 }
 
 
+export const initMemoryGame = meta => {
+
+	meta.board = new Board(false, params.frame);
+	meta.board.init();
+	meta.game = new Game(meta.board);
+	meta.terminated = false;
+
+	return meta;
+
+}
+
+
 const boxMullerRandom = () => {
 	let u = 0, v = 0;
-	while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+	while (u === 0) u = Math.random(); //Converting [0,1) to (0,1) ]
 	while (v === 0) v = Math.random();
 	let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
 
@@ -42,4 +54,26 @@ export const normRand = (mean, std) => {
 
 	return mean + boxMullerRandom() * std;
 
+}
+
+
+// This is staying here.
+export const softmax = (z, temperature = 1) => {
+	let max = Math.max(...z);
+	let scaledScores = z.map(score => Math.exp((score - max) / temperature));
+	let sumScaledScores = scaledScores.reduce((sum, val) => sum + val, 0);
+	return scaledScores.map(score => score / sumScaledScores);
+}
+
+
+export const probRand = (probabilities) => {
+    let cumulativeSum = 0;
+    let r = Math.random();
+    for (let i = 0; i < probabilities.length; i++) {
+        cumulativeSum += probabilities[i];
+        if (r < cumulativeSum) {
+            return i;
+        }
+    }
+    return probabilities.length - 1;  // In case of rounding errors, return the last index
 }
